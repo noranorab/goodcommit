4dfb15fbc219975f2ede9fe4f3dc9b0a48bac626
noranorab <noranorab7@gmail.com>
2024-01-15 00:02:00 +0100

    added retrieve commits from git repo

--- main.cpp
+++ main.cpp
@@ -4,14 +4,35 @@ using namespace std;
 
 int main(){
     cout << "Welcome to GoodCommit" << endl;
-    // std::cout << "Let's Start !" << std::endl;
-    // std::cout << "Enter your Git Repository :" << std::endl;
-    // std::cin >> std::endl;
-
-    // prompt the user for entering the repo
     git_libgit2_init();
-    
-    cout << "Welcome to GoodCommit " << endl;
+    const char* repo_path = "https://github.com/noranorab/mini-projet";
 
+    git_repository* git_repo = nullptr;
+    int error = git_repository_open(&git_repo, repo_path);
+
+    if (error < 0)
+    {
+        cout << "Error opening the repository" << endl;
+    }
+
+    git_revwalk* walker = nullptr;
+    git_revwalk_new(&walker, git_repo);
+    git_revwalk_push_head(walker);
+
+    git_oid commit_oid;
+    while (git_revwalk_next(&commit_oid, walker)) 
+    {
+        git_commit* commit = nullptr;
+        git_commit_lookup(&commit, git_repo, &commit_oid);
+
+        cout << "Commit ID:" << git_oid_tostr_s(&commit_oid) <<endl;
+        git_commit_free(commit);
+    }
+
+    
+    
+    git_revwalk_free(walker);
+    git_repository_free(git_repo);
+    git_libgit2_shutdown();
     return 0;
 }
