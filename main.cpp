0c2e501cca36d9744b895d82f027601af3e465fb
noranorab <noranorab7@gmail.com>
2024-01-22 14:15:59 +0100

    added tokenize funciton

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <git2.h>
+#include <clang-c/Index.h>
+#include <vector>
 using namespace std;
 
 
@@ -105,35 +107,76 @@ void identifyCodeChanges(git_repository* repo)
 }
 
 int print_patch_line(
-    git_diff_delta *delta,       // Delta information
-    git_diff_hunk *hunk,         // Hunk information
-    git_diff_line *line,         // Line information
+    const git_diff_delta *delta,       // Delta information
+    const git_diff_hunk *hunk ,         // Hunk information
+    const git_diff_line *line,         // Line information
     void *payload                // Custom payload (can be nullptr)
 ) {
     // Example: Print the content of each line
-    std::cout << "Line: " << line->content << std::endl;
 
+    
+    std::cout << "Line Status: ";
+    
+    switch (line->origin) {
+        case GIT_DIFF_LINE_ADDITION:
+            std::cout << "Added";
+            break;
+        case GIT_DIFF_LINE_DELETION:
+            std::cout << "Deleted";
+            break;
+        default:
+            std::cout << "Modified";
+    }
+
+    std::cout << ", Content: " << line->content << std::endl;
     // Return 0 to continue processing lines
     return 0;
 }
 
 void print_diff(git_diff *diff)
 {
-    size_t nums_deltas = size_t num_deltas = git_diff_num_deltas(diff);
+    size_t nums_deltas = git_diff_num_deltas(diff);
+    cout << "Number of Deltas: " << nums_deltas << endl;
 
     for (size_t i=0; i < nums_deltas; ++i)
     {
         const git_diff_delta *delta = git_diff_get_delta(diff, i);
         out("File : ", delta->new_file.path);
 
-        git_patch *patch;
+        git_patch *patch = nullptr;
+
         if (git_patch_from_diff(&patch, diff, i) == GIT_OK)
         {
+            cout << "--------------------------------------------" << endl;
             git_patch_print(patch, print_patch_line, nullptr);
             git_patch_free(patch);
         }
         std::cout << std::endl;
     }
+}
+
+size_t getTheNumberOfChangedLines(git_patch* patch, int num)
+{
+    return git_patch_num_lines_in_hunk(patch, num);
+}
+
+void getTheContentOfEachChangedLine(git_patch* patch, size_t num_lines)
+{
+    vector<string> changedLines;
+    for (size_t i=0; i<num_lines; ++i)
+    {
+        const char* lineContent = git_patch_line_in_hunk(patch, 0, i, nullptr, nullptr, nullptr);
+        changedLines.emplace_back(lineContent);
+    }
+}
+
+void tokenizeChangedLines(const char* filePath, const vector<string>& changedLines)
+{
+    // Create index
+    CXIndex index = clang_createIndex(0, 0);
+    // Parse the file
+    // Tokenize ad traverse the AST for each changed line
+    // Dispose of the translation
 }
 
 
@@ -145,10 +188,33 @@ int main(){
     int error = openGitRepo(&git_repo, "/home/nora/Bureau/goodcommit/goodcommit");
 
     handleError(error, "Error opening the repository");
-
+    
     identifyCodeChanges(git_repo);
     
+    //Get the HEAD commit
+    git_oid head_oid;
+    git_reference_name_to_id(&head_oid, git_repo, "HEAD");
     
+
+    git_commit *head_commit = nullptr;
+    git_commit_lookup(&head_commit, git_repo, &head_oid);
+    git_tree *head_tree;
+    git_commit_tree(&head_tree, head_commit);
+
+    git_index *index = nullptr;
+    git_repository_index(&index, git_repo);
+
+    git_diff *diff = nullptr;
+    git_diff_index_to_workdir(&diff, git_repo, index, nullptr);
+
+    
+    print_diff(diff);
+    cout << "i am here " << endl;
+
+    git_diff_free(diff);
+    git_index_free(index);
+    git_tree_free(head_tree);
+    git_commit_free(head_commit);
     git_repository_free(git_repo);
     git_libgit2_shutdown();
     return 0;
